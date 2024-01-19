065b995484fb5d1576858877c79502468d0d8337
daaayeonn <yooda1234567@naver.com>
2024-01-19 15:05:20 +0900

    1단계 1차

--- src/banking/BankingSystemMain.java
+++ src/banking/BankingSystemMain.java
@@ -6,19 +6,41 @@ public class BankingSystemMain {
 	
 	public static void showMenu() {
 		System.out.println("-----Menu-----");
-		System.out.println("1. 계 좌 개 설");
-		System.out.println("2. 입	  금");
-		System.out.println("3. 출	  금");
+		System.out.println("1. 계좌개설");
+		System.out.println("2. 입금");
+		System.out.println("3. 출금");
 		System.out.println("4. 계좌정보출력");
 		System.out.println("5. 프로그램종료");
+		System.out.print("메뉴를 선택하세요: ");
 	}
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		AccountInfoHandler handler = new AccountInfoHandler();
 
-		while (true) {
+		while(true) {
 			showMenu();
-		}
-	}
+			
+			int choice = scan.nextInt();
+			
+			switch(choice) {
+			case ICustomDefine.MAKE:
+				handler.makeAccount();
+				break;
+			case ICustomDefine.DEPOSIT:
+				handler.depositMoney();
+				break;
+			case ICustomDefine.WITHDRAW:
+				handler.withdrawMoney();
+				break;
+			case ICustomDefine.INQUIRE:
+				handler.showAccInfo();
+				break;
+			case ICustomDefine.EXIT:
+				 System.out.println("프로그램 종료");
+				return;
+			} // switch 끝
+		} // while 끝
+	} // main 끝
 
 }
